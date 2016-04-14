c555765c21ceeb3da865d9b302e10f630e13c6dd
Sreenivasa <sreenivasa.krishnamurthy@gmail.com>
2016-04-14 13:34:41 +0100

    Pom dependencies added for junit

--- test/com/shopping/fruit/FruitTest.java
+++ test/com/shopping/fruit/FruitTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.shopping.visitor.ShoppingItemVisitor;
-import com.shopping.visitor.ShoppingItemVisitorImpl;
 
 public class FruitTest {
 
